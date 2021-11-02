8fa56c1a674dd2bc0d45f2a4d3a93d387034c40b
noa <noa.trachez@epitech.eu>
2021-11-02 14:10:02 +0100

    ~Div must be tested with mul

--- src/compute/div/div.c
+++ src/compute/div/div.c
@@ -25,20 +25,31 @@ void search_who_is_upper_n(char **ten_n, char **ten_n_b, char *r)
 
 void search_who_is_upper_c(char **ten_n_b, char *r, int *c)
 {
+    char *c_char;
+
     while (my_strcmp(*ten_n_b, r) <= 0) {
-        ten_n_b = mul(*ten_n_b, *c);
+        int_to_str(*c, *c_char);
+        ten_n_b = mul(*ten_n_b, c_char);
         *c += 1;
     }
 }
 
-void replace_add()
+void replace_add(char **q, char *ten_n, int c)
 {
+    char *c_char;
 
+    int_to_str(c, *c_char);
+    ten_n = mul(ten_n, c_char);
+    *q = infin_add(*q, ten_n);
 }
 
-void replace_sub()
+void replace_sub(char **r, char *ten_n_b, int c)
 {
+    char *c_char;
 
+    int_to_str(c, *c_char);
+    ten_n_b = mul(ten_n_b, c_char);
+    *r = infin_sub(*r, ten_n_b);
 }
 
 char *div(char *a, char *b)
@@ -53,12 +64,14 @@ char *div(char *a, char *b)
         write(2, "error", 5);
         exit;
     }
-    while (my_strcmp(r, sec) >= 0) {
+    while (my_strcmp(r, b) >= 0) {
         search_who_is_upper_n(&ten_n, &ten_n_b, r);
         search_who_is_upper_c(&ten_n_b, r, &c);
-        replace_add(q, ten_n, c);
-        replace_sub(r, ten_n_b, c);
+        replace_add(&q, ten_n, c);
+        replace_sub(&r, ten_n_b, c);
     }
+    my_putstr(q);
+    my_putstr(r);
 }
 
 int main(int ac, char **argv)
