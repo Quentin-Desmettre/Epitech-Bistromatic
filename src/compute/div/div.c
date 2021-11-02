1d1ca5d10cf306e80f04f04a0698edc8230ffba7
noa <noa.trachez@epitech.eu>
2021-11-02 18:46:18 +0100

    Div inf complete without base

--- src/compute/div/div.c
+++ src/compute/div/div.c
@@ -34,26 +34,28 @@ void search_who_is_upper_n(char **ten_n, char **ten_n_b, char *r,
         append_char(&tmp_n, '0', 1);
     }
     len = my_strlen(tmp_n);
-    if (len > 1) {
+    if (len > 1)
         tmp_n[len - 1] = '\0';
+    len = my_strlen(tmp);
+    if (len > 1)
         tmp[len - 1] = '\0';
-    }
     *ten_n = tmp;
     *ten_n_b = tmp_n;
 }
 
-char *search_who_is_upper_c(char **ten_n_b, char *r, int *c, expr_params_t *par)
+char *search_who_is_upper_c(char *ten_n_b, char *r, int *c, expr_params_t *par)
 {
     char *c_char;
-    char *ten_n_bc = *ten_n_b;
+    char *ten_n_bc = ten_n_b;
 
-    *c = 2;
-    while (my_nbr_cmp(ten_n_bc, r, par->base) <= 0) {
-        int_to_str(*c, &c_char);
-        ten_n_bc = infin_mul(ten_n_bc, c_char, par->base, par->ops);
-        *c += 1;
+    for (int i = 2; my_nbr_cmp(ten_n_bc, r, par->base) <= 0; i++) {
+        int_to_str(i, &c_char);
+        ten_n_bc = infin_mul(ten_n_b, c_char, par->base, par->ops);
+        int_to_str(i - 1, &c_char);
     }
-    return ten_n_bc;
+    *c = my_getnbr(c_char);
+    ten_n_bc = infin_mul(ten_n_b, c_char, par->base, par->ops);
+    return (ten_n_bc);
 }
 
 void replace_add(char **q, char *ten_n, int c, expr_params_t *par)
@@ -85,14 +87,13 @@ char *infin_div(char *a, char *b, char *base, char *ops)
         exit(84);
     }
     while ((my_nbr_cmp(r, b, base) >= 0) && r[0] != '-') {
+        ten_n_b = my_strdup(b);
+        ten_n = my_strdup("1");
+        ten_n_bc = my_strdup("1");
         search_who_is_upper_n(&ten_n, &ten_n_b, r, &par);
-        ten_n_bc = search_who_is_upper_c(&ten_n_bc, r, &c, &par);
-        printf("I'm ten_n_bc %s\n", ten_n_bc);
-        printf("I'm c %d\n", c);
+        ten_n_bc = search_who_is_upper_c(ten_n_b, r, &c, &par);
         replace_add(&q, ten_n, c, &par);
-        printf("I'm q %s\n", q);
         replace_sub(&r, ten_n_bc, &par);
-        printf("I'm r %s\n", r);
     }
     return (q);
 }
