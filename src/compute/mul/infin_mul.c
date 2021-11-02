5fc394f64cc8b8ef910b7e3f0de4698795890457
Clement <clement.vandeville@epitech.eu>
2021-11-02 15:11:47 +0100

    rm index_of duplicata

--- src/compute/mul/infin_mul.c
+++ src/compute/mul/infin_mul.c
@@ -18,15 +18,6 @@ void re_alloc(char **str, char *new, int is_free)
         free(tmp);
 }
 
-int index_of(char c, char *str)
-{
-    for (int i = 0; str[i]; i++) {
-        if (str[i] == c)
-            return i;
-    }
-    return -1;
-}
-
 void init_with_int(unsigned long long *array, int what, int len)
 {
     for (int i = 0; i < len; i++) {
@@ -34,23 +25,30 @@ void init_with_int(unsigned long long *array, int what, int len)
     }
 }
 
+ull *main_loop(ull *tmp, char *fir, char *sec, char *base) {
+    int len_i = my_strlen(fir);
+    int len_j = my_strlen(sec);
+    int len_base = my_strlen(base);
+    int len = my_strlen(fir) + my_strlen(sec) + 2;
+
+    tmp[len - i - j - 1] += index_of(fir[len_i - i - 1], base) *
+    index_of(sec[len_j - j - 1], base);
+    tmp[len - i - j - 2] += tmp[len - i - j - 1] / len_base;
+    tmp[len - i - j - 1] %= len_base;
+    return (tmp);
+}
+
 char *infin_mul(char *fir, char *sec, char *base, char *ops)
 {
     int len = my_strlen(fir) + my_strlen(sec) + 2;
     ull *tmp = malloc(sizeof(ull) * (len + 1));
     char *result = malloc(len + 1);
-    int len_i = my_strlen(fir);
-    int len_j = my_strlen(sec);
-    int len_base = my_strlen(base);
 
     init_with_int(tmp, 0, len + 1);
     my_memset(result, 0, len + 1);
     for (int i = 0; fir[i]; i++)
         for (int j = 0; sec[j]; j++) {
-            tmp[len - i - j - 1] += index_of(fir[len_i - i - 1], base) *
-            index_of(sec[len_j - j - 1], base);
-            tmp[len - i - j - 2] += tmp[len - i - j - 1] / len_base;
-            tmp[len - i - j - 1] %= len_base;
+            tmp = main_loop(tmp, fir, sec, base);
         }
     for (int i = 0; i < len; i++)
         result[i] = base[tmp[i]];
