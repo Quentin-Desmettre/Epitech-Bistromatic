2c5bda14f0b1d36683bd8f2922ac222f95c82c88
noa <noa.trachez@epitech.eu>
2021-11-04 12:37:51 +0100

    unit test_evaluate computeraw

--- tests/test_evaluate_simple.c
+++ tests/test_evaluate_simple.c
@@ -2,19 +2,20 @@
 ** EPITECH PROJECT, 2021
 ** Bistro
 ** File description:
-** TODO: add description
+** Bistro
 */
 
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 #include "bistromatic.h"
 
-
-char *compute_raw(int start, int *len, int *len_1, expr_params_t *par)
-Test(compute_raw, do_good_cal)
+Test(compute_raw, do_good_call)
 {
-    expr_params_t par = {"5-8+5*5", "0123456789", "()+-*/%"};
-    int start = 2;
-    int len;
+    int start = 5;
+    int len = 0;
+    int len_first = 0;
+    char *calc = my_strdup("5-8+5*5");
 
+    expr_params_t par = {calc, "0123456789", "()+-*/%"};
+    cr_assert_str_eq(compute_raw(start, &len, &len_first, &par), "25");
 }
