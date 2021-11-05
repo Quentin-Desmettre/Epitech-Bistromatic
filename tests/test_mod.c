ccdb537e3e4d7f0b774689dfbfbe3e5ed195d646
Clement <clement.vandeville@epitech.eu>
2021-11-05 20:50:24 +0100

    Added new tests for test_mod.c

--- tests/test_mod.c
+++ tests/test_mod.c
@@ -18,3 +18,32 @@ Test (check_null, check_if_null)
     result = "56";
     cr_assert_str_neq(check_null(&result, base), "0");
 }
+
+Test (my_mod, check_nice_mod)
+{
+    char *a = "-5967";
+    char *b = "12";
+    char *base = "0123456789";
+    char *ops = "()+-*/%";
+
+    cr_assert_str_eq(my_mod(a + 1, b, base, ops), "3");
+    cr_assert_str_eq(my_mod(a, b, base, ops), a);
+}
+
+Test (infin_mod, check_all_mod)
+{
+    char *a = "-5697";
+    char *b = "-12";
+    char *result;
+    char * base = "0123456789";
+    char *ops = "()+-*/%";
+
+    result = infin_mod(a, b, base, ops);
+    cr_assert_str_eq(result, "-9");
+    result = infin_mod(a + 1, b, base, ops);
+    cr_assert_str_eq(result, "9");
+    result = infin_mod(a + 1, b + 1, base, ops);
+    cr_assert_str_eq(result, "9");
+    result = infin_mod(a, b + 1, base, ops);
+    cr_assert_str_eq(result, "-9");
+}
