3931388fc15b09e4cb5c41f7e750c67b65fd82f8
Clement <clement.vandeville@epitech.eu>
2021-11-05 19:37:38 +0100

    Added 2 new test for clean_expr

--- tests/test_clean_expr.c
+++ tests/test_clean_expr.c
@@ -38,3 +38,17 @@ Test (clean_expr, remove_zeros, .init = redirect_all_stdout)
     cr_assert_str_eq(expr, "78");
 }
 
+Test (clean_expr, is_rm_zero, .init = redirect_all_stdout)
+{
+    char *expr = my_strdup("789");
+
+    cr_assert(is_rm_zero(expr, "0123456789", 0) == 1);
+}
+
+Test (clean_expr, cleanex, .init = redirect_all_stdout)
+{
+    char *expr = my_strdup("+--++00789");
+
+    cleanex(&expr, "0123456789", "()+-*/%");
+    cr_assert_str_eq(expr, "789");
+}
