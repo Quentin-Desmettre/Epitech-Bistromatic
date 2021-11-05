f5a9b923323789395143464b9fa897b7ea48d622
Clement <clement.vandeville@epitech.eu>
2021-11-05 19:18:37 +0100

    New tests clean expr

--- tests/test_clean_expr.c
+++ tests/test_clean_expr.c
@@ -25,6 +25,9 @@ Test (clean_expr, replace_negs, .init = redirect_all_stdout)
     expr = my_strdup("8+---+78");
     (replace_negs(&expr, "0123456789", "()+-*/%", 1));
     cr_assert_str_eq(expr, "8-78");
+    expr = my_strdup("8+(55)--78");
+    (replace_negs(&expr, "0123456789", "()+-*/%", 6));
+    cr_assert_str_eq(expr, "8+(55)+78");
 }
 
 Test (clean_expr, remove_zeros, .init = redirect_all_stdout)
@@ -35,17 +38,3 @@ Test (clean_expr, remove_zeros, .init = redirect_all_stdout)
     cr_assert_str_eq(expr, "78");
 }
 
-Test (clean_expr, is_rm_zero, .init = redirect_all_stdout)
-{
-    char *expr = my_strdup("789");
-
-    cr_assert(is_rm_zero(expr, "0123456789", 0) == 1);
-}
-
-Test (clean_expr, cleanex, .init = redirect_all_stdout)
-{
-    char *expr = my_strdup("+--++00789");
-
-    cleanex(&expr, "0123456789", "()+-*/%");
-    cr_assert_str_eq(expr, "789");
-}
