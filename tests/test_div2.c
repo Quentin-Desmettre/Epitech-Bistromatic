141e935b2848618e93d99e2de38bf3f91aa9c5d9
Clement <clement.vandeville@epitech.eu>
2021-11-06 15:23:17 +0100

    Added a test to test_div + Updated Makefile

--- tests/test_div2.c
+++ tests/test_div2.c
@@ -23,3 +23,21 @@ Test (my_div, do_div)
     result = my_div(a, b, &par);
     cr_assert_str_eq(result, "0");
 }
+
+Test (inf_div, all_possibility)
+{
+    char *a = "-5697";
+    char *b = "-12";
+    char *result;
+    char * base = "0123456789";
+    char *ops = "()+-*/%";
+
+    result = infin_div(a, b, base, ops);
+    cr_assert_str_eq(result, "474");
+    result = infin_div(a + 1, b, base, ops);
+    cr_assert_str_eq(result, "-474");
+    result = infin_div(a + 1, b + 1, base, ops);
+    cr_assert_str_eq(result, "474");
+    result = infin_div(a, b + 1, base, ops);
+    cr_assert_str_eq(result, "-474");
+}
